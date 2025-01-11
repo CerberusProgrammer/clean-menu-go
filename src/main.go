e4a0d43e25237a8f01cbc875437a19108e14a0f4
sazar <sazardev@gmail.com>
2025-01-11 10:24:25 -0800

    Add HTTP file server and base HTML layout template

--- src/main.go
+++ src/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+
+	// src\ui\static
+	fileserver := http.FileServer(http.Dir("src/ui/static"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
 }
