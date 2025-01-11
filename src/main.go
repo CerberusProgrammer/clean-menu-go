11e47397b9f9316e3060857f8728969f372a3d8f
sazar <sazardev@gmail.com>
2025-01-11 11:18:07 -0800

    Implement home page with navigation and styling

--- src/main.go
+++ src/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"sazardev.clean-menu-go/src/web"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 	mux := http.NewServeMux()
 
 	// src\ui\static
 	fileserver := http.FileServer(http.Dir("src/ui/static"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+	mux.HandleFunc("/", web.Home)
 
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", mux)
 }
---
+++ src/web/home_controller.go
@@ -0,0 +1,38 @@
+package web
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		filepath.Join("src", "ui", "pages", "home.tmpl.html"),
+		filepath.Join("src", "ui", "layouts", "layout.tmpl.html"),
+		filepath.Join("src", "ui", "components", "nav.component.html"),
+	}
+
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Println(err.Error())
+		fmt.Fprintf(w, "Unable to load template")
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+
+	if err != nil {
+		log.Println(err.Error())
+		fmt.Fprintf(w, "Unable to render template")
+		return
+	}
+}
