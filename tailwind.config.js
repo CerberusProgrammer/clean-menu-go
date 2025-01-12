26db66fe65c05d64662ce16c6d5bdceefbcb205f
sazar <sazardev@gmail.com>
2025-01-12 11:36:44 -0800

    Add initial project setup with Tailwind CSS configuration and layout files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/ui/**/*.html',
+    './src/ui/**/*.tmpl',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
