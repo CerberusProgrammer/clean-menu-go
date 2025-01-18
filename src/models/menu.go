bc59dcdd8f59aa062af7f3abee89432a0fdd4770
Omar Flores <dev@dev.com>
2025-01-17 17:24:02 -0800

    Add categories and image fields to Menu model

--- src/models/menu.go
+++ src/models/menu.go
@@ -1,11 +1,12 @@
 package models
 
 type Menu struct {
-	ID       int     `json:"id"`
-	Name     string  `json:"name"`
-	Price    float64 `json:"price"`
-	Recipe   string  `json:"recipe"`
-	Category string  `json:"category"`
+	ID         int      `json:"id"`
+	Name       string   `json:"name"`
+	Price      float64  `json:"price"`
+	Recipe     string   `json:"recipe"`
+	Categories []string `json:"categories"`
+	Image      string   `json:"image"`
 }
 
 var Menus []Menu
