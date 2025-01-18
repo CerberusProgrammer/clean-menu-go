731d335405b709ad86955119401e58368765cbad
Omar Flores <dev@dev.com>
2025-01-17 16:16:24 -0800

    Add authentication package with functions to set and get the current user

---
+++ src/auth/auth.go
@@ -0,0 +1,13 @@
+package auth
+
+import "sazardev.clean-menu-go/src/models"
+
+var CurrentUser models.User
+
+func SetCurrentUser(user models.User) {
+	CurrentUser = user
+}
+
+func GetCurrentUser() models.User {
+	return CurrentUser
+}
