3518cd0d49975217285c051fdd7c148bf42aa1a7
sazardev <sazardev@gmail.com>
2025-01-20 06:18:44 -0800

    Enhance Menu model: add description, availability, estimated time, ingredients, and timestamps

--- src/models/menu.go
+++ src/models/menu.go
@@ -1,12 +1,21 @@
 package models
 
+import "time"
+
 type Menu struct {
-	ID         int      `json:"id"`
-	Name       string   `json:"name"`
-	Price      float64  `json:"price"`
-	Recipe     string   `json:"recipe"`
-	Categories []string `json:"categories"`
-	Image      string   `json:"image"`
+	ID            int       `json:"id"`
+	Name          string    `json:"name"`
+	Price         float64   `json:"price"`
+	Recipe        string    `json:"recipe"`
+	Categories    []string  `json:"categories"`
+	Image         string    `json:"image"`
+	Description   string    `json:"description"`
+	Availability  bool      `json:"availability"`
+	EstimatedTime int       `json:"estimated_time"`
+	Ingredients   []string  `json:"ingredients"`
+	CreatedBy     User      `json:"created_by"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
 
 var Menus []Menu
